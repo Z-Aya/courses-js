011ed44b1bc47840db33fad60b171000b290b95c
Z-Aya <121736460+Z-Aya@users.noreply.github.com>
2023-01-01 14:25:56 +0100

    Add files via upload

---
+++ db.js
@@ -0,0 +1,127 @@
+var courses = [{
+        image: './dir/javascriptDef.png',
+        title: 'what is javascript?',
+        category: 'JS',
+        price: 9.9
+    },
+    {
+        image: './dir/htmlBasics.PNG',
+        title: 'basics of HTML',
+        category: 'HTML',
+        price: 5.9
+    },
+    {
+        image: './dir/python-data.png',
+        title: 'Python for Data science',
+        category: 'python',
+        price: 5.9
+    },
+    {
+        image: './dir/htmlElements.png',
+        title: 'HTML elements and tags',
+        category: 'HTML',
+        price: 9.9
+    },
+    {
+        image: './dir/cssSelectors.jpg',
+        title: 'CSS selectors',
+        category: 'CSS',
+        price: 69.9
+    },
+    {
+        image: './dir/javascriptVariables.png',
+        title: 'variables and data type of javascript',
+        category: 'JS',
+        price: 19.9
+    },
+    {
+        image: './dir/javascriptOperators.png',
+        title: 'Javascript operators and conditions',
+        category: 'JS',
+        price: 29.9
+    },
+    {
+        image: './dir/htmlAttrVal.jpg',
+        title: 'HTML attributes and values',
+        category: 'HTML',
+        price: 19.9
+    },
+    {
+        image: './dir/cssProperties.png',
+        title: 'CSS properties',
+        category: 'CSS',
+        price: 29.9
+    },
+    {
+        image: './dir/javascriptObjects.png',
+        title: 'Javascript objects and arrays',
+        category: 'JS',
+        price: 39.9
+    },
+    {
+        image: './dir/cssMesures.png',
+        title: 'mesures and unites of CSS',
+        category: 'CSS',
+        price: 19.9
+    },
+    {
+        image: './dir/cssAnimation.png',
+        title: 'CSS animations',
+        category: 'CSS',
+        price: 19.9
+    },
+    {
+        image: './dir/javascriptFunctions.png',
+        title: 'The basics of javascript',
+        category: 'JS',
+        price: 29.9
+    },
+    {
+        image: './dir/javascriptEvents.png',
+        title: 'javascript events',
+        category: 'JS',
+        price: 59.9
+    },
+    {
+        image: './dir/cssColors.png',
+        title: 'css colors',
+        category: 'css',
+        price: 29.9
+    },
+    {
+        image: './dir/phpBasics.png',
+        title: 'getting started with php',
+        category: 'php',
+        price: 15.9
+    },
+    {
+        image: './dir/javascriptFunctions2.png',
+        title: 'functions and loops with javascript',
+        category: 'JS',
+        price: 19.9
+    },
+    {
+        image: './dir/phpDataBase.png',
+        title: 'connecting to database using PHP',
+        category: 'PHP',
+        price: 29.9
+    },
+    {
+        image: './dir/phpCRUD.png',
+        title: 'manipulating crud using php',
+        category: 'php',
+        price: 45.9
+    },
+    {
+        image: './dir/javascriptDOM.png',
+        title: 'DOM the power of javascript',
+        category: 'JS',
+        price: 23.9
+    },
+    {
+        image: './dir/javascriptDOM.png',
+        title: 'DOM the power of javascript',
+        category: 'JS_g4',
+        price: 23.9
+    }
+]
